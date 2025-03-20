7c264102a0a37c6d1019ce1e7f38851c319185fd
sshyta <sshyta@mail.ru>
2025-03-20 23:39:36 +0300

    Thermo Humidity

---
+++ ThermoHumidity.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <DHT.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define DHTPIN 2         
+#define DHTTYPE DHT11     
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(57600);
+  lcd.init();
+  lcd.backlight();
+  dht.begin();
+  lcd.setCursor(0, 0);
+  lcd.print("Темп: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Влаж: ");
+}
+
+void loop() {
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  lcd.setCursor(6, 0);
+  lcd.print(t);
+  lcd.print(" C");
+  lcd.setCursor(6, 1);
+  lcd.print(h);
+  lcd.print(" %");
+
+  Serial.print("Температура: ");
+  Serial.print(t);
+  Serial.print(" *C\t");
+  Serial.print("Влажность: ");
+  Serial.print(h);
+  Serial.println(" %");
+  delay(2000);
+}
